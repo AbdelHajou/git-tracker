b36030754dd10b2a212d5c1f7eda87d6900ba774
Abdel Hajou <abdelwhajou@gmail.com>
2021-07-01 23:38:42 +0200

    Added missing test for Commit

---
+++ src/commits/Commit.test.js
@@ -0,0 +1,16 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import Commit from './Commit'
+
+const commit = {
+    sha: 'c937aabbd7f46fa3b283744169a6b48dafbd6cc7',
+    message: 'Introduce new bugs\n\nI felt that the project needed more bugs, so I added some'
+};
+
+test('should show commit info', async () => {
+    render(<Commit key={commit.sha} commit={commit} repo={{ name: 'mocktocat/reposteel' }} />);
+
+    await waitFor(() => {
+        expect(screen.getByText('Introduce new bugs')).toBeInTheDocument();
+        expect(screen.getByText('mocktocat/reposteel')).toBeInTheDocument();
+    });
+});
