13e3664f028fa3f5c7cf74a38185952405afac5f
AbdelHajou <abdelwhajou@gmail.com>
2021-07-03 20:49:51 +0200

    Paginate commit history (closes #13)
    Improve styling of profile page and add pagination to commit history

--- src/commits/CommitHistory.js
+++ src/commits/CommitHistory.js
@@ -1,17 +1,30 @@
 import { useEffect, useState } from 'react';
 import Commit from './Commit';
+import List from '@material-ui/core/List';
+import { ListItem } from '@material-ui/core';
+import { Button } from 'react-bootstrap';
+import './CommitHistory.css';
 
 const CommitHistory = ({ eventsUrl }) => {
+    const commitIncrements = 15;
     const [events, setEvents] = useState([]);
+    const [numberOfCommits, setNumberOfCommits] = useState(commitIncrements);
+    const [showLoadMore, setShowLoadMore] = useState(true);
 
     useEffect(() => {
         const getEvents = async () => {
-            const events = await fetchEvents(eventsUrl)
-            setEvents(events.filter(event => event.type === 'PushEvent'));
+            var gitEvents = await fetchEvents(eventsUrl)
+            gitEvents = gitEvents.filter(event => event.type === 'PushEvent');
+            
+            gitEvents.forEach(event => {
+                event.payload.commits.forEach(commit => commit.repoName = event.repo.name);
+            });
+
+            setEvents(gitEvents);
         }
 
         getEvents()
-    }, [eventsUrl])
+    }, [eventsUrl]);
 
     const fetchEvents = async (eventsUrl) => {
         const res = await fetch(eventsUrl)
@@ -19,15 +32,36 @@ const CommitHistory = ({ eventsUrl }) => {
         return data
     }
 
+    const loadMoreCommits = async () => {
+        var actualNumberOfCommits = events.flatMap(event => event.payload.commits).length;
+        if (numberOfCommits + commitIncrements > actualNumberOfCommits) {
+            setNumberOfCommits(actualNumberOfCommits);
+            setShowLoadMore(false);
+        } else {
+            setNumberOfCommits(numberOfCommits + commitIncrements);
+        }
+    };
+
     return (
         <div className='commitHistory'>
-            <h2>Commit history</h2>
             {events.length === 0 ? 'This user has not committed to any public repositories yet.' :
-            events.map(event => (
-                event.payload.commits.map(commit => (
-                    <Commit key={commit.sha} commit={commit} repo={event.repo} />
-                ))
-            ))}
+                (
+                    <List dense style={{ paddingTop: '0'}}>
+                        {
+                            events.flatMap(event => event.payload.commits).slice(0, numberOfCommits).map(commit => (
+                                <Commit key={commit.sha} commit={commit} repoName={commit.repoName} />
+                            ))
+                        }
+                        { showLoadMore &&
+                        <ListItem id='loadMore'>
+                            <Button onClick={loadMoreCommits} style={{margin: 'auto', backgroundColor: '#f14e32', borderColor: '#f14e32'}}>
+                            Load more
+                            </Button>
+                        </ListItem>
+                        }
+                    </List>
+                )
+            }
         </div>
     )
 }
